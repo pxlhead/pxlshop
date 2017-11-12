672e361b590671a14c924728b4d202c6dfdec2dc
Liza Kobrazova <lizakobrazova@gmail.com>
2017-11-12 19:11:31 +0300

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -10,11 +8,9 @@ module.exports = {
     browser: true,
   },
   extends: 'airbnb-base',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // check if imports actually resolve
   'settings': {
     'import/resolver': {
       'webpack': {
@@ -22,18 +18,25 @@ module.exports = {
       }
     }
   },
-  // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
       'vue': 'never'
     }],
-    // allow optionalDependencies
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: ['state']
+    }],
+    'no-shadow': ['error', {
+      'allow': [
+        'state',
+        'getters'
+      ]
+    }],
+    'comma-dangle': ['error', 'never'],
     'import/no-extraneous-dependencies': ['error', {
       'optionalDependencies': ['test/unit/index.js']
     }],
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
